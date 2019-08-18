b7a8292b5ee79b99642d8b42f8899694404bb94c
arn3342 <nabilrashid44@gmail.com>
2019-08-18 19:50:21 +0600

    Simplified code

--- Users.DbAccess/Tools/FilterBuilder.cs
+++ Users.DbAccess/Tools/FilterBuilder.cs
@@ -14,110 +14,116 @@ namespace Users.DbAccess.Tools
         /// Generates a <see cref="MongoDB.Driver.UpdateDefinition{TDocument}"/> of any given type.
         /// </summary>
         /// <example>
-        /// If you want to update a parent Object/object :
+        /// To update a parent object, call the function as follows:
         /// <code>
         /// var parentObject = new ParentObject() { Id = 123, First_Name = "Brian };
         /// var comparingObjectObject = objectReturnedFromDatabase
-        /// var childObject = new ChildObject() { Id = 456, Field_1 = "Test" }
-        /// FilterBuilder filterOptions = new FilterBuilder();
-        /// var UpdateDefinition<ParentClass>FilterBuilder.ToUpdate<ParentClass>(parentObject, comparingObjectObject);
+        /// 
+        /// FilterBuilder fl = new FilterBuilder();
+        /// var Upadte = fl.ToUpdate<ParentClass>(parentObject, comparingObjectObject);
         /// </code>
-        /// If you want to update a child object/Object inside a parent object/Object, call the function as follows :
-        /// <code>
-        /// var UpdateDefinition<ChildClass>FilterBuilder.ToUpdate<ChildClass>(parentObject, comparingObjectObject, childObject);
-        /// </code>
-        /// If you want to update an array. call the function as follows :
-        /// <code>
-        /// /// To update an array of a parent 
-        /// var UpdateDefinition<ChildClass>FilterBuilder.ToUpdate<ChildClass>(parentObject, Child: childObject, ArrayProperty: parentObject.ArrayPropertyName);
-        /// </code>
-        /// </example>
         /// <typeparam name="T"></typeparam>
-        /// <param name="Parent">The parent object</param>
-        /// <param name="comparingObject">The comparing object(optional)</param>
-        /// <param name="Child">The child object(optional)</param>
-        /// <param name="ArrayProperty">The array property of the parent/child to update.</param>
-        /// <returns>An <c>UpdateDefition</c> of any given type.</returns>
-        public UpdateDefinition<T> ToUpdate<T>(object Parent, object comparingObject = null, object Child = null, object ArrayProperty = null)
+        /// <param name="Parent">The object to update.</param>
+        /// <param name="comparingObject">The comparing object. This is basically the document returned from the collection.</param>
+        /// <returns>An <see cref="UpdateDefinition{TDocument}"> of any given type.</returns>
+        public UpdateDefinition<T> ToUpdate<T>(object Parent, object comparingObject)
         {
             UpdateDefinitionBuilder<T> update_filter = Builders<T>.Update;
             List<UpdateDefinition<T>> updates = new List<UpdateDefinition<T>>();
 
-            #region Not updaing an array
-            if (ArrayProperty == null)
+            foreach (var property in Parent.GetType().GetProperties())
             {
-                #region Modifying a child document
-                if (Child != null)
-                {
-                    foreach (var property in Child.GetType().GetProperties())
-                    {
-                        var value = property.GetValue(Child);
-                        var oldValue = property.GetValue(comparingObject);
-                        var CheckDefault = new object();
-                        if (value == null)
-                        {
-                            CheckDefault = ValueChecker.ConvertObjectToString(value);
-                        }
-                        else { CheckDefault = ValueChecker.GetDefaultValue(value.GetType()); }
+                var value = property.GetValue(Parent);
+                var oldValue = property.GetValue(comparingObject);
+                var CheckDefault = new object();
 
-                        if (value != CheckDefault)
-                        {
-                            if (oldValue == null) oldValue = ValueChecker.ConvertObjectToString(oldValue);
-                            if (value != null && value.ToString() != oldValue.ToString() && value.ToString() != CheckDefault.ToString())
-                            {
-                                string field_name = property.Name;
-                                updates.Add(update_filter.Set(Child.GetType().Name + "." + field_name, value));
-                            }
-                        }
+                if (value == null)
+                {
+                    CheckDefault = ValueChecker.ConvertObjectToString(value);
+                }
+                else { CheckDefault = ValueChecker.GetDefaultValue(value.GetType()); }
+
+                if (value != CheckDefault)
+                {
+                    if (oldValue == null) oldValue = ValueChecker.ConvertObjectToString(oldValue);
+                    if (value != null && value.ToString() != oldValue.ToString() && value.ToString() != CheckDefault.ToString())
+                    {
+                        string field_name = property.Name;
+                        updates.Add(update_filter.Set(field_name, value));
                     }
                 }
-                #endregion
 
-                #region Modifying the parent document.
-                else
-                {
-                    foreach (var property in Parent.GetType().GetProperties())
-                    {
-                        var value = property.GetValue(Parent);
-                        var oldValue = property.GetValue(comparingObject);
-                        var CheckDefault = new object();
-
-                        if (value == null)
-                        {
-                            CheckDefault = ValueChecker.ConvertObjectToString(value);
-                        }
-                        else { CheckDefault = ValueChecker.GetDefaultValue(value.GetType()); }
-
-                        if (value != CheckDefault)
-                        {
-                            if (oldValue == null) oldValue = ValueChecker.ConvertObjectToString(oldValue);
-                            if (value != null && value.ToString() != oldValue.ToString() && value.ToString() != CheckDefault.ToString())
-                            {
-                                string field_name = property.Name;
-                                updates.Add(update_filter.Set(field_name, value));
-                            }
-                        }
-
-                    }
-                }
-                #endregion
             }
-            #endregion
-
-            #region Updating an Array
-            else
-            {
-                updates.Add(update_filter.Push(ArrayProperty.GetType().Name + ".", Child));
-            }
-            #endregion
             return update_filter.Combine(updates);
         }
 
-        public UpdateDefinition<T> ToUpdate<T>(object Parent, object comparingChildObject, object Child)
-        { return null; }
+        /// <summary>
+        /// An overload of <see cref="ToUpdate{T}(object, object)"/> to generate a <see cref="MongoDB.Driver.UpdateDefinition{TDocument}"/> of a child object.
+        /// </summary>
+        /// <example>
+        /// To update a child object, call the function as follows:
+        /// <code>
+        /// var childObject = new ChildObject() { Id = 456, Child_First_Name = "Aousaf };
+        /// var comparingObjectObject = childObjectReturnedFromDatabase
+        /// 
+        /// FilterBuilder fl = new FilterBuilder();
+        /// var Upadte = fl.ToUpdate<ParentClass>(childObject, childObjectReturnedFromDatabase, true);
+        /// </code>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Parent">The object to update.</param>
+        /// <param name="comparingObject">The comparing object. This is basically the document returned from the collection.</param>
+        /// <returns>An <see cref="UpdateDefinition{TDocument}"> of any given type.</returns>
+        public UpdateDefinition<T> ToUpdate<T>(object Parent, object comparingObject, bool IsChild)
+        {
+            UpdateDefinitionBuilder<T> update_filter = Builders<T>.Update;
+            List<UpdateDefinition<T>> updates = new List<UpdateDefinition<T>>();
+            foreach (var property in Parent.GetType().GetProperties())
+            {
+                var value = property.GetValue(Parent);
+                var oldValue = property.GetValue(comparingObject);
+                var CheckDefault = new object();
+                if (value == null)
+                {
+                    CheckDefault = ValueChecker.ConvertObjectToString(value);
+                }
+                else { CheckDefault = ValueChecker.GetDefaultValue(value.GetType()); }
 
-        public UpdateDefinition<T> ToUpdate<T>(object Child, object ArrayProperty)
-        { return null;  }
+                if (value != CheckDefault)
+                {
+                    if (oldValue == null) oldValue = ValueChecker.ConvertObjectToString(oldValue);
+                    if (value != null && value.ToString() != oldValue.ToString() && value.ToString() != CheckDefault.ToString())
+                    {
+                        string field_name = property.Name;
+                        updates.Add(update_filter.Set(Parent.GetType().Name + "." + field_name, value));
+                    }
+                }
+            }
+            return update_filter.Combine(updates);
+        }
+
+        /// <summary>
+        /// An overload of <see cref="ToUpdate{T}(object, object)"/> to generate a <see cref="MongoDB.Driver.UpdateDefinition{TDocument}"/> to update an array of child objects.
+        /// </summary>
+        /// <example>
+        /// To update an array of child objects, call the function as follows:
+        /// <code>
+        /// var childObject = new ChildObject() { Id = 456, Child_First_Name = "Aousaf };
+        /// var array = ParentObject.ArrayProperty;
+        /// 
+        /// FilterBuilder fl = new FilterBuilder();
+        /// var Upadte = fl.ToUpdate<ParentClass>(childObject, array);
+        /// </code>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Parent">The object to update.</param>
+        /// <param name="ArrayProperty">The array property of the parent.</param>
+        /// <returns>An <see cref="UpdateDefinition{TDocument}"> of any given type.</returns>
+        public UpdateDefinition<T> ToUpdate<T>(object Parent, Array ArrayProperty)
+        {
+            UpdateDefinitionBuilder<T> update_filter = Builders<T>.Update;
+            List<UpdateDefinition<T>> updates = new List<UpdateDefinition<T>>();
+            updates.Add(update_filter.Push(ArrayProperty.GetType().Name, Parent));
+            return update_filter.Combine(updates);
+        }
 
         public FilterDefinition<T> ToFind<T>(string KeyPropertyName, object Value)
         {
@@ -125,7 +131,7 @@ namespace Users.DbAccess.Tools
             {
                 Value = new ObjectId(Value.ToString());
             }
-            
+
             FilterDefinition<T> query_filter = Builders<T>.Filter.Eq(x => x.GetType().GetProperty(KeyPropertyName).GetValue(x), Value);
 
             return query_filter;
